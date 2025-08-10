a545b8ae3fdba28840e722bbbe100a8d390b3a70
Rahul <bantaramrohit@gmail.com>
2025-08-10 22:42:02 +0530

    Create main.tf

---
+++ TerraformVM/main.tf
@@ -0,0 +1,65 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.prefix}-nic"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "${var.prefix}-vm"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = var.os_image.publisher
+    offer     = var.os_image.offer
+    sku       = var.os_image.sku
+    version   = var.os_image.version
+  }
+
+  os_disk {
+    caching           = "ReadWrite"
+    #create_option     = "FromImage"
+    storage_account_type = "Standard_LRS"
+  }
+
+  #os_profile {
+  #admin_username = var.admin_username
+  #admin_password = var.admin_password
+  #}
+  #os_profile_linux_config {
+  #  disable_password_authentication = false
+  #}
+
+  tags = {
+    environment = "dev"
+  }
+}
